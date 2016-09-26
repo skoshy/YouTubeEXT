63798a207d17981d112b944069e828f38db1a322
Stefan Koshy <noreply@skoshy.com>
2016-09-26 10:20:01 -0400

    Fix for versions

--- userscript.js
+++ userscript.js
@@ -2,7 +2,7 @@
 // @name         YouTube Supreme
 // @icon         https://www.youtube.com/yt/brand/media/image/YouTube-icon-full_color.png
 // @namespace    skoshy.com
-// @version      0.55
+// @version      0.57
 // @description  Does cool things with YouTube
 // @author       Stefan Koshy
 // @match        http*://*.youtube.com/*
